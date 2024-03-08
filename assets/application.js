c7667224b2f8f769d923910a4f097f64d01bb797
Tixon <tixon24@gmail.com>
2024-03-08 18:02:50 +0200

    upd

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,20 @@
 // Put your application javascript here
+/* sliders START */
+var custom_slider = [];
+$(document).find('.posts-slider').each(function(el) {
+    let _slider = $(this).find('.swiper');
+    let _scrollbar = $(this).find('.slider-scrollbar');
+
+    custom_slider[el] = new Swiper(_slider[0], {
+        slidesPerView: 'auto',
+        spaceBetween: 45,
+        observer: true,
+        watchOverflow: true,
+        scrollbar: {
+            el: _scrollbar[0],
+            draggable: true,
+            hide: false,
+        },
+    });
+});
+/* sliders END */
